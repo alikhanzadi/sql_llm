247a11a49dd247dae620996d985af847bdbd373a
alikhanzadi <khanzadi.ali@gmail.com>
2026-04-27 01:26:15 -0700

    Updates to query runner to be able to use both local and cloud databases.

--- app/db/query_runner.py
+++ app/db/query_runner.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
 import streamlit as st 
@@ -8,14 +9,31 @@ from psycopg2.extras import RealDictCursor
 
 class PostgresClient:
     def __init__(self):
-        # This single block works for BOTH local and cloud 
-        # because Streamlit handles the 'st.secrets' lookup automatically.
-        creds = st.secrets["postgres"]
-        
-        # Check if we are on the cloud (Neon) or local (Docker)
-        # Neon (cloud) usually requires SSL
+
+        # Decide environment (local vs prod)
+        db_env = os.getenv("DB_ENV", "local")
+
+        try:
+            key = "postgres_neon" if db_env == "prod" else "postgres_local"
+            creds = st.secrets[key]
+        except Exception:
+            # fallback for local CLI runs
+            creds = {
+                "host": os.getenv("POSTGRES_HOST"),
+                "port": os.getenv("POSTGRES_PORT"),
+                "database": os.getenv("POSTGRES_DB"),
+                "user": os.getenv("POSTGRES_USER"),
+                "password": os.getenv("POSTGRES_PASSWORD"),
+            }
+        print(os.getenv("POSTGRES_HOST"))
+
+        # # 1. Get the credentials (wherever they are)
+        # creds = st.secrets["postgres"]
+
+        # SSL for Neon only
         ssl_mode = "require" if "neon.tech" in creds["host"] else "disable"
 
+        # 3. Connect using the dynamically chosen SSL mode
         self.conn = psycopg2.connect(
             host=creds["host"],
             port=creds["port"],
@@ -23,28 +41,7 @@ class PostgresClient:
             user=creds["user"],
             password=creds["password"],
             sslmode=ssl_mode
-        )
-    
-    #     # 1. Try to get secrets from Streamlit Cloud
-    #     if "postgres" in st.secrets:
-    #         creds = st.secrets["postgres"]
-    #         self.conn = psycopg2.connect(
-    #             host=creds["host"],
-    #             port=creds["port"],
-    #             database=creds["database"],
-    #             user=creds["user"],
-    #             password=creds["password"],
-    #             sslmode="require" # Cloud DBs usually need this
-    #         )
-    #     # 2. Fallback to your local Docker settings
-    #     else:
-    #         self.conn = psycopg2.connect(
-    #             host="localhost",
-    #             port=5432,
-    #             dbname="analytics_db",
-    #             user="admin",
-    #             password="admin"
-    #         )
+        )        
 
     def run_query(self, query: str):
         try:
@@ -52,6 +49,7 @@ class PostgresClient:
                 cur.execute(query)
                 return cur.fetchall()
         except Exception as e:
+            self.conn.rollback()
             return {"error": str(e)}
 
 if __name__ == "__main__":
--- app/llm/generate_sql.py
+++ app/llm/generate_sql.py
@@ -2,10 +2,6 @@ import os
 from openai import OpenAI
 from dotenv import load_dotenv
 
-# Mostly Day 5
-
-# Day 15: Why this is removed: keyword-based filtering is removed and replaced with semantic retrieval (RAG)
-# from app.schema import get_schema # Day 15 - commented out
 from .prompts import SYSTEM_PROMPT
 
 from app.rag.retriever import retrieve_relevant_docs
@@ -19,14 +15,6 @@ SYNONYMS = {
     "trades": ["transactions"]
 }
 
-# def format_schema(schema: dict) -> str: # Day 15 - commented out
-#     text = ""
-#     for table, cols in schema.items():
-#         text += f"\nTable: {table}\n"
-#         for col in cols:
-#             text += f"- {col}\n"
-#     return text
-
 def clean_sql(response_text: str) -> str:
     return (
         response_text
@@ -34,117 +22,6 @@ def clean_sql(response_text: str) -> str:
         .replace("```", "")
         .strip()
     )
-
-def matches(term: str, text: str) -> bool:
-    return (
-        term in text or
-        term.rstrip("s") in text or
-        (term + "s") in text
-    )
-
-# def filter_schema(schema: dict, question: str) -> dict: # DAY 5 # Day 9 --> commented out
-#     question = question.lower()
-#     filtered = {}
-
-#     for table, cols in schema.items():
-#         table_name = table.lower()
-
-#         # include synonyms
-#         synonyms = SYNONYMS.get(table_name, [])
-#         all_terms = [table_name] + synonyms
-
-#         # table_match = any(str(term) in question for term in all_terms) #this was before we used matches to resolve prular vs singluar
-#         table_match = any(matches(term, question) for term in all_terms)
-
-#         col_matches = [
-#             col for col in cols if col.lower() in question
-#         ]
-
-#         if table_match or col_matches:
-#             filtered[table] = cols
-
-#     print("TERMS:", all_terms)
-#     print("MATCHED TABLE:", table if table_match else None)
-
-    # fallback if nothing matched
-    # return filtered if filtered else schema
-
-# def filter_schema(schema: dict, question: str) -> dict: #Day 9 # Day 15 - commented out
-#     question = question.lower()
-#     table_scores = {}
-
-#     for table, cols in schema.items():
-#         table_name = table.lower()
-
-#         # include synonyms
-#         synonyms = SYNONYMS.get(table_name, [])
-#         all_terms = [table_name] + synonyms
-#         score = 0
-
-#         # Table name match
-#         if any(matches(term, question) for term in all_terms):
-#             score += 3
-
-#         # Column matches
-#         for col in cols:
-#             col_clean = col.lower().replace("_", " ")
-#             if matches(col_clean,question):
-#             # col.lower() in question:
-#                 print(col)
-#                 score += 1
-
-#         # store score if relevant
-#         if score > 0:
-#             table_scores[table] = score
-
-#     # sort tables by score
-#     sorted_tables = sorted(
-#         table_scores.items(),
-#         key=lambda x: x[1],
-#         reverse=True
-#     )
-
-#     # keep top N tables (important)
-#     TOP_K = 3
-#     selected_tables = [t[0] for t in sorted_tables[:TOP_K]]
-
-#     filtered = {t: schema[t] for t in selected_tables}
-
-#     print("TABLE SCORES:", table_scores)
-#     print("SELECTED TABLES:", selected_tables)
-
-#     return filtered if filtered else schema
-
-# def generate_sql(user_query: str) -> str: # Day 5 | # Day 15 - commented out
-#     schema = get_schema()
-#     filtered_schema = filter_schema(schema, user_query)
-#     schema_text = format_schema(filtered_schema)
-
-#     prompt = f"""
-#     Database Schema:
-#     {schema_text}
-
-#     User Question:
-#     {user_query}
-#     """
-
-#     response = client.chat.completions.create(
-#         model="gpt-4o-mini",
-#         messages=[
-#             {"role": "system", "content": SYSTEM_PROMPT},
-#             {"role": "user", "content": prompt}
-#         ],
-#         temperature=0
-#     )
-
-#     raw_sql = response.choices[0].message.content
-#     cleaned_sql = clean_sql(raw_sql)
-    
-#     print("\n--- FILTERED SCHEMA ---")
-#     print(schema_text)
-#     print("--------------\n")
-
-#     return cleaned_sql
 
 def generate_sql(user_query: str) -> str:
     # Step 1: Retrieve relevant schema docs
